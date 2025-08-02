bd87b89ddb17ac5cbd5207be6d26656ef591eeff
Vlad <Vladyslav10@yahoo.com>
2025-08-02 15:01:17 +0100

    first somewhat working prototype (MVP-minimal viable product)

--- apicall.py
+++ apicall.py
@@ -7,21 +7,50 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-
-
-
-
+# Initialising OpenAI client with API key from environment variable (hidden for security)
 client = OpenAI(
     api_key= os.getenv("APIKEY")
 )
 
-# response = client.responses.create(
-#     model="gpt-4.1-nano",
-#     instructions="You are a coding assistant that talks like a pirate.",
-#     input="How do I check if a Python object is an instance of a class?",
-# )
+user_input = "What is this document for?"
 
-#print(response.output_text)
 
-# with open("weightandmeasuresact.pdf", "r") as f:
-#             f = f.read()
+# Uploading a file to OpenAI for use
+with open("OAPA.pdf", "rb") as f:
+    file = client.files.create(file=f, purpose="assistants")
+    file_id = file.id
+
+
+# Creating a vector store (RAG) and uploading the file to it
+vs = client.vector_stores.create(name="Simple Vector Store")
+client.vector_stores.files.create_and_poll(vector_store_id=vs.id, file_id=file_id)
+
+
+# Going into the vectore store in order to query it further
+results = client.vector_stores.search(
+        vector_store_id=vs.id,
+        query=user_input, # Query line to search for vector
+        rewrite_query=True,
+        max_num_results=10
+    )
+
+print(results)
+
+def extract_text(results):
+    return "\n".join(
+        chunk.text
+        for result in results.data
+        for chunk in result.content
+    )
+
+# MVP = Minimal Viable Product
+
+messages = [{"role": "system", "content": "You are a helpful legal assistant. Use the provided legal document context to answer the user's question."}] # Content of the system message - can be modified to change how the model will answer
+messages.append({"role": "user", "content": f"Context:\n{(extract_text(results))}\n\nQuestion: {user_input}"})
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=messages
+    )
+
+print(response.choices[0].message.content)
